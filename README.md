8d52f1ef142707a77d4494f3b807db9d4c4673fa
coryrwest <cory.r.west@gmail.com>
2015-12-12 12:19:21 -0800

    Inital Commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# B2.NET
+
+B2.NET is a C# client for the Backblaze B2 Cloud Storage service through its REST API.
+
+B2.NET is still in early Alpha, so use it in production at your own risk.
+
+## Features
+
+*  Full implmentation of the B@ REST API.
+*  Full Async support (Planned)
+
+## NuGet
+
+B2.NET will eventually be available as a NuGet package.
+
+## Install
+
+Use the NuGet package, or just stick the B2.NET.ddl in your project.
+
+## Examples
+
+Coming.
+
+## Release Notes
+
+0.1.0 Initial Alpha release.
+
---
+++ src/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("B2.NET")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("B2.NET")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5161362e-59b9-4959-8701-59feef469eca")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
