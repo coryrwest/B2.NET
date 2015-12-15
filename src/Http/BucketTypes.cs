af1763651718d199d946c6fb4cb6dc44bdc1bde7
Cory Westropp <coryrwest@gmail.com>
2015-12-14 16:17:37 -0800

    Finished Bucket API. Built up readme a little.

---
+++ src/Http/BucketTypes.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B2Net.Http {
+	public enum BucketTypes {
+		allPublic,
+		allPrivate
+	}
+}
