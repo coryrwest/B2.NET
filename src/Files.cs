053e791206fdc80ecb23a4be60a85196c50a035c
Cory Westropp <coryrwest@gmail.com>
2015-12-17 14:32:33 -0800

    File upload API complete

--- src/Files.cs
+++ src/Files.cs
@@ -39,12 +39,55 @@ namespace B2Net {
 			var response = await client.SendAsync(requestMessage, cancelToken);
 
 			var jsonResponse = await response.Content.ReadAsStringAsync();
-			if (response.IsSuccessStatusCode) {
-				var fileList = JsonConvert.DeserializeObject<B2FileList>(jsonResponse);
-				return fileList;
-			} else {
-				throw new AuthorizationException(jsonResponse);
+
+			Utilities.CheckForErrors(response);
+
+			return JsonConvert.DeserializeObject<B2FileList>(jsonResponse);
+		}
+
+		/// <summary>
+		/// Uploads one file to B2, returning its unique file ID.
+		/// </summary>
+		/// <param name="fileData"></param>
+		/// <param name="fileName"></param>
+		/// <param name="bucketId"></param>
+		/// <param name="cancelToken"></param>
+		/// <returns></returns>
+		public async Task<B2File> Upload(byte[] fileData, string fileName, string bucketId = "", CancellationToken cancelToken = default(CancellationToken)) {
+			// Check for a persistant bucket
+			if (!_options.PersistBucket && string.IsNullOrEmpty(bucketId)) {
+				//throw new ArgumentNullException(nameof(bucketId));
 			}
+
+			var client = HttpClientFactory.CreateHttpClient();
+
+			// Are we persisting buckets? If so use the one from settings
+			string operationalBucketId = _options.PersistBucket ? _options.BucketId : bucketId;
+
+			// Get the upload url for this file
+			// TODO: There must be a better way to do this
+			var uploadUrlRequest = FileUploadRequestGenerators.GetUploadUrl(_options, operationalBucketId);
+			var uploadUrlResponse = client.SendAsync(uploadUrlRequest, cancelToken).Result;
+			var uploadUrlData = await uploadUrlResponse.Content.ReadAsStringAsync();
+			var uploadUrlObject = JsonConvert.DeserializeObject<B2UploadUrl>(uploadUrlData);
+			// Set the upload auth token
+			_options.UploadAuthorizationToken = uploadUrlObject.AuthorizationToken;
+
+			// Now we can upload the file
+			var requestMessage = FileUploadRequestGenerators.Upload(_options, uploadUrlObject.UploadUrl, fileData, fileName);
+			var response = await client.SendAsync(requestMessage, cancelToken);
+
+			var jsonResponse = await response.Content.ReadAsStringAsync();
+
+			Utilities.CheckForErrors(response);
+
+			return JsonConvert.DeserializeObject<B2File>(jsonResponse);
+		}
+
+		internal class B2UploadUrl {
+			public string BucketId { get; set; }
+			public string UploadUrl { get; set; }
+			public string AuthorizationToken { get; set; }
 		}
 	}
 }
--- src/Http/RequestGenerators/FileUploadRequestGenerators.cs
+++ src/Http/RequestGenerators/FileUploadRequestGenerators.cs
@@ -1,15 +1,40 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using B2Net.Http.RequestGenerators;
+using B2Net.Models;
 
 namespace B2Net.Http {
 	public static class FileUploadRequestGenerators {
 		private static class Endpoints {
-			public const string Upload = "b2_upload_file";
 			public const string GetUploadUrl = "b2_get_upload_url";
 		}
 
+		public static HttpRequestMessage Upload(B2Options options, string uploadUrl, byte[] fileData, string fileName) {
+			var uri = new Uri(uploadUrl);
+			var request = new HttpRequestMessage() {
+				Method = HttpMethod.Post,
+				RequestUri = uri,
+				Content = new ByteArrayContent(fileData)
+			};
+
+			// Get the file checksum
+			string hash = Utilities.GetSHA1Hash(fileData);
+
+			// Add headers
+			request.Headers.Add("Authorization", options.UploadAuthorizationToken);
+			request.Headers.Add("X-Bz-File-Name", fileName);
+			request.Headers.Add("X-Bz-Content-Sha1", hash);
+			//request.Headers.Add("X-Bz-Info-src_last_modified_millis", fileName);
+
+			request.Content.Headers.ContentType = new MediaTypeHeaderValue("b2/x-auto");
+			request.Content.Headers.ContentLength = fileData.Length;
+
+			return request;
+		}
+
+		public static HttpRequestMessage GetUploadUrl(B2Options options, string bucketId) {
+			return BaseRequestGenerator.PostRequest(FileUploadRequestGenerators.Endpoints.GetUploadUrl, "{\"bucketId\":\"" + bucketId + "\"}", options);
+		}
 	}
 }
