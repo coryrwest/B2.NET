cf7d558415f2e71a6c2c0f52a361e6c5cbf92898
Cory Westropp <github@crwest.com>
2015-12-17 14:32:07 -0800

    Refactoring names for clarity and conciseness.

--- src/Models/B2FileList.cs
+++ src/Models/B2FileList.cs
@@ -7,6 +7,6 @@ using System.Threading.Tasks;
 namespace B2Net.Models {
 	public class B2FileList {
 		public string NextFileName { get; set; }
-		public List<B2File> FIles { get; set; }
+		public List<B2File> Files { get; set; }
 	}
 }
