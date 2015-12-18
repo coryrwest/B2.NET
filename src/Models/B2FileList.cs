8aff9af55870b9a18c9d7b37b838ed0720ce09cf
Cory Westropp <coryrwest@gmail.com>
2015-12-18 15:50:43 -0800

    Get File Versions endpoint

--- src/Models/B2FileList.cs
+++ src/Models/B2FileList.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 namespace B2Net.Models {
 	public class B2FileList {
 		public string NextFileName { get; set; }
+		public string NextFileId { get; set; }
 		public List<B2File> Files { get; set; }
 	}
 }
--- tests/FileTests.cs
+++ tests/FileTests.cs
@@ -69,6 +69,19 @@ namespace B2Net.Tests {
 
 		[TestMethod]
 		public void ListVersionsTest() {
+			var fileName = "B2Test.txt";
+			var fileData = File.ReadAllBytes(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, fileName));
+			string hash = Utilities.GetSHA1Hash(fileData);
+			var file = Client.Files.Upload(fileData, fileName, TestBucket.BucketId).Result;
+
+			Assert.AreEqual(hash, file.ContentSHA1, "File hashes did not match.");
+			
+			var versions = Client.Files.GetVersions(file.FileName, file.FileId, bucketId: TestBucket.BucketId).Result;
+
+			// Clean up. We have to delete the file before we can delete the bucket
+			var deletedFile = Client.Files.Delete(file.FileId, file.FileName).Result;
+
+			Assert.AreEqual(1, versions.Files.Count);
 		}
 
 		[TestMethod]
