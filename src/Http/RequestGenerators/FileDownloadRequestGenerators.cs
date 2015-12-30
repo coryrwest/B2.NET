afacef2da75a550bbc764981a7a760321f25fe39
Cory Westropp <github@crwest.com>
2015-12-30 10:32:15 -0800

    File download endpoints. Initial API complete.

--- src/Http/RequestGenerators/FileDownloadRequestGenerators.cs
+++ src/Http/RequestGenerators/FileDownloadRequestGenerators.cs
@@ -1,15 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using B2Net.Http.RequestGenerators;
+using B2Net.Models;
 
 namespace B2Net.Http {
 	public static class FileDownloadRequestGenerators {
 		private static class Endpoints {
 			public const string DownloadById = "b2_download_file_by_id";
-			public const string DownloadByName = "b2_download_file_by_name";
+			public const string DownloadByName = "file";
 		}
 
+		public static HttpRequestMessage DownloadById(B2Options options, string fileId) {
+			var uri = new Uri(options.DownloadUrl + "/b2api/" + Constants.Version + "/" + Endpoints.DownloadById);
+			var request = new HttpRequestMessage() {
+				Method = HttpMethod.Post,
+				RequestUri = uri,
+				Content = new StringContent("{\"fileId\":\"" + fileId + "\"}")
+			};
+
+			request.Headers.Add("Authorization", options.AuthorizationToken);
+
+			return request;
+		}
+
+		public static HttpRequestMessage DownloadByName(B2Options options, string bucketName, string fileName) {
+			var uri = new Uri(options.DownloadUrl + "/" + Endpoints.DownloadByName + "/" + bucketName + "/" + fileName);
+			var request = new HttpRequestMessage() {
+				Method = HttpMethod.Get,
+				RequestUri = uri
+			};
+
+			request.Headers.Add("Authorization", options.AuthorizationToken);
+
+			return request;
+		}
 	}
 }
