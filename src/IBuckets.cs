036d2e26b5463efa0db19703063d4ad10779a720
Matt Dwen <matt@dwen.co.nz>
2018-06-05 15:15:30 +1200

    Add IBuckets interface

---
+++ src/IBuckets.cs
@@ -0,0 +1,17 @@
+﻿using B2Net.Models;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace B2Net
+{
+  public interface IBuckets
+  {
+    Task<B2Bucket> Create(string bucketName, B2BucketOptions options, CancellationToken cancelToken = default(CancellationToken));
+    Task<B2Bucket> Create(string bucketName, BucketTypes bucketType, CancellationToken cancelToken = default(CancellationToken));
+    Task<B2Bucket> Delete(string bucketId = "", CancellationToken cancelToken = default(CancellationToken));
+    Task<List<B2Bucket>> GetList(CancellationToken cancelToken = default(CancellationToken));
+    Task<B2Bucket> Update(B2BucketOptions options, string bucketId = "", CancellationToken cancelToken = default(CancellationToken));
+    Task<B2Bucket> Update(BucketTypes bucketType, string bucketId = "", CancellationToken cancelToken = default(CancellationToken));
+  }
+}
