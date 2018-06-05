98b57812b3c699f1c92872a73b8715089c99aafe
Matt Dwen <matt@dwen.co.nz>
2018-06-05 15:16:22 +1200

    Use interfaces in B2Net.B2Client

--- src/B2Client.cs
+++ src/B2Client.cs
@@ -34,9 +34,9 @@ namespace B2Net {
 	        LargeFiles = new LargeFiles(_options);
         }
 
-		public Buckets Buckets { get; }
-	    public Files Files { get; }
-	    public LargeFiles LargeFiles { get; }
+		public IBuckets Buckets { get; }
+	    public IFiles Files { get; }
+	    public ILargeFiles LargeFiles { get; }
 
         /// <summary>
         /// Authorize against the B2 storage service. Requires that AccountId and ApplicationKey on the options object be set.
